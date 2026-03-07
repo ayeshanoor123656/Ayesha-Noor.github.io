c73fb60d07b5510f2e9ac2912fac79c818dd245f
ayeshanoor123656 <ayeshaayeshanoor346@gmail.com>
2026-03-07 12:30:58 +0500

    add

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const navLinks = document.querySelectorAll('nav ul li a');
 
 navLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
+    link.addEventListener('click', function(e){
         e.preventDefault();
         const target = document.querySelector(link.getAttribute('href'));
         target.scrollIntoView({ behavior: 'smooth' });
